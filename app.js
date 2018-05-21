0a0bf6d7d52e322946f6ad94ceab009a55939233
kwmiebach <kwmiebach@gmail.com>
2018-05-21 19:07:10 +0200

    Finally index.html and the js

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+
+
+import {html, render} from 'lit-html';
+
+// This is a lit-html template function. It returns a lit-html template.
+const helloTemplate = (name) => html`<div>Hello ${name}!</div>`;
+
+// Call the function with some data, and pass the result to render()
+
+// This renders <div>Hello Steve!</div> to the document body
+render(helloTemplate('Steve'), document.body);
+
+// This updates to <div>Hello Kevin!</div>, but only updates the ${name} part
+render(helloTemplate('Kevin'), document.body);
+
+
+
