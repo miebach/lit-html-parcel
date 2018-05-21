c173f90a0c60ceec0867f0131596b4afad63ec81
kwmiebach <kwmiebach@gmail.com>
2018-05-21 19:44:42 +0200

    Render 2 versions of the same template in a loop every 1 second

--- app.js
+++ app.js
@@ -8,11 +8,19 @@ const helloTemplate = (name) => html`<div>Hello ${name}!</div>`;
 
 // Call the function with some data, and pass the result to render()
 
-// This renders <div>Hello Steve!</div> to the document body
-render(helloTemplate('Steve'), document.body);
 
-// This updates to <div>Hello Kevin!</div>, but only updates the ${name} part
-render(helloTemplate('Kevin'), document.body);
+function state1 () {
+  // This renders <div>Hello Steve!</div> to the document body
+  render(helloTemplate('Steve'), document.body);
+  setTimeout(state2, 1000); 
+}
 
+function state2 () {
+  // This updates to <div>Hello Kevin!</div>, but only updates the ${name} part
+  render(helloTemplate('Kevin'), document.body);
+  setTimeout(state1, 1000); 
+}
+
+state1();
 
 
