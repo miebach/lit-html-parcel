b3c7e85913884cd6df9017d068ec3e4933d05d1b
kwmiebach <kwmiebach@gmail.com>
2018-05-23 16:52:49 +0200

    GIVING UP ON JASMINE
    it seems impossible to choose a different directory structure

--- jasmine/example/PlayerSpec.js
+++ jasmine/example/PlayerSpec.js
@@ -1,6 +1,6 @@
 describe("Player", function() {
-  var Player = require('../../lib/jasmine_examples/Player');
-  var Song = require('../../lib/jasmine_examples/Song');
+  var Player = require('./Player');
+  var Song = require('./Song');
   var player;
   var song;
 
