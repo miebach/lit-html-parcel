b0d85b1be3018481740f4544c065ffc2c9796146
kwmiebach <kwmiebach@gmail.com>
2018-06-08 02:16:32 +0200

    prepare 2 page templates
    and remove setTimeout demo

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
 console.log("hello world");
 
-
 import {html, render} from 'lit-html';
 
 // This is a lit-html template function. It returns a lit-html template.
-const helloTemplate = (name) => html`<div>Hello ${name}!</div>`;
+const pageTemplate = (name) => html`<div>This is the ${name} page!</div>`;
 
 // Call the function with some data, and pass the result to render()
 
-
-function state1 () {
-  // This renders <div>Hello Steve!</div> to the document body
-  render(helloTemplate('Steve'), document.body);
-  setTimeout(state2, 1000); 
+function page1 () {
+  render(pageTemplate('first'), document.body);
 }
 
-function state2 () {
-  // This updates to <div>Hello Kevin!</div>, but only updates the ${name} part
-  render(helloTemplate('Kevin'), document.body);
-  setTimeout(state1, 1000); 
+function page2 () {
+  render(pageTemplate('second'), document.body);
 }
 
-state1();
-
-
+page1();
